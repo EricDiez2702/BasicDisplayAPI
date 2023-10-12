b30a870e442b767d33f11da150db6efe3c0b53e7
EricDiez2702 <147709611+EricDiez2702@users.noreply.github.com>
2023-10-12 14:19:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ st.write(instagram_basic_display.get_login_url())
 
 API_URL = "https://api.instagram.com/oauth/access_token"
 data = {
-    "client_id":{631172465595715},
-    "client_secret":{74c6eb8f791cb341222af420802db667},
+    "client_id":{'631172465595715'},
+    "client_secret":{'74c6eb8f791cb341222af420802db667'},
     "grant_type": "authorization_code",
     "redirect_uri"={https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/},
-    "code"={AQDcuAB6IP1-U8eZMno7cuzZO4nkTgNOHyImkIFYaeJ1-mkOk551Heb1b0L0PGi-EW0T_uRPBeuttvS_3N59coz1DgRZ-ABJPdbjMYE0cP0MBPVcKZnRfEspEkqGAdbx15l9jxfOoA6iWqrWZ6jZuuJ13Z49Okg1AIZaYO6GFFCKLogWD4GLI8cs7O9mTRfguo2w8-2UiAkb0spzNcFrV-xXxe3NCBiHZZr_kKvR7HeRCw&state=6f6da625-e4c2-492b-b40c-84337274ec39}
+    "code"={'AQDcuAB6IP1-U8eZMno7cuzZO4nkTgNOHyImkIFYaeJ1-mkOk551Heb1b0L0PGi-EW0T_uRPBeuttvS_3N59coz1DgRZ-ABJPdbjMYE0cP0MBPVcKZnRfEspEkqGAdbx15l9jxfOoA6iWqrWZ6jZuuJ13Z49Okg1AIZaYO6GFFCKLogWD4GLI8cs7O9mTRfguo2w8-2UiAkb0spzNcFrV-xXxe3NCBiHZZr_kKvR7HeRCw&state=6f6da625-e4c2-492b-b40c-84337274ec39'}
 }
 
 response = requests.post(API_URL, data)
