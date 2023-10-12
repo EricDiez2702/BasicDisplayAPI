ed0c73ab5e0b79f7fb7265bc92844612f195886f
EricDiez2702 <147709611+EricDiez2702@users.noreply.github.com>
2023-10-12 13:21:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ from instagram_basic_display.InstagramBasicDisplay import InstagramBasicDisplay
 st.title("The AI knows you better")
 instagram_basic_display = InstagramBasicDisplay(app_id='631172465595715', app_secret='74c6eb8f791cb341222af420802db667', redirect_url='https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/')
 st.write(instagram_basic_display.get_login_url())
+
+# Get the OAuth callback code
+code = request.args.get('code')
+
+# Get the short lived access token (valid for 1 hour)
+short_lived_token = instagram_basic_display.get_o_auth_token(code)
+
+# Exchange this token for a long lived token (valid for 60 days)
+long_lived_token = instagram_basic_display.get_long_lived_token(short_lived_token.get('access_token'))
+
+print('Your token is: {}' .format(long_lived_token.access_token))
