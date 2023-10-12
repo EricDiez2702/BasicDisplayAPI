2d2f80ef354a00abfe60c34332d4d61734a4fe96
EricDiez2702 <147709611+EricDiez2702@users.noreply.github.com>
2023-10-12 14:37:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,18 @@ st.title("The AI knows you better")
 instagram_basic_display = InstagramBasicDisplay(app_id='631172465595715', app_secret='74c6eb8f791cb341222af420802db667', redirect_url='https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/')
 st.write(instagram_basic_display.get_login_url())
 
-API_URL = "https://api.instagram.com/oauth/access_token"
-data = {
-    "client_id":'631172465595715',
-    "client_secret":'74c6eb8f791cb341222af420802db667',
-    "grant_type": "authorization_code",
-    "redirect_uri":'https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/',
-    "code":'AQCiIXlY2tWvitBb84v0wv6crFHm-kKEGS5xrGJ8TjidUpdj6fIlkpZPBKmtXvtZevua4OXeAN1aHV_LaHXSPLbz9Go7fJsaK5tSc4jvQUP6B2cu2BCOj3_c1ipRmJaTuJ2Mgo-pbF1yA8xlzY5nzsey_SEAb228WjwrSBSAAHxY3meR6lY61dfd7_OhoS2uNqtcGXiggkO6K0d9FNmQtr08L_bcH-w2NhcsXDZYNa5dQQ&state=60d6c5e9-3d9b-411c-81f8-9d6fc69dc529'
-}
+code = st.text_input("Entrez le code d'authentification :")
 
-response = requests.post(API_URL, data)
-st.write(response.json())
+if code =! null:
+    API_URL = "https://api.instagram.com/oauth/access_token"
+    data = {
+        "client_id":'631172465595715',
+        "client_secret":'74c6eb8f791cb341222af420802db667',
+        "grant_type": "authorization_code",
+        "redirect_uri":'https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/',
+        "code":code
+    }
+    
+    response = requests.post(API_URL, data)
+    st.write(response.json())
+
