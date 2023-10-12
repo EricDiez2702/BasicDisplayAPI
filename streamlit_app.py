173df85b2a9c2c2607fde5c7ad7418577a97355a
EricDiez2702 <147709611+EricDiez2702@users.noreply.github.com>
2023-10-12 16:29:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,10 +29,12 @@ if code_recu:
     }
     
     response1 = requests.post(apiUrl, data)
-    st.write(response1.json())
+    #st.write(response1.json())
 
-    user_id = st.text_input("Entrer Id utilisateur :")
-    token = st.text_input("Entrer le token :")
+    data = response1.json()
+
+    user_id = data.get('user_id')
+    token = data.get('access_token')
     fields = 'id,username'
     
     token_recu = st.checkbox("Echange du code contre un token réussi")
