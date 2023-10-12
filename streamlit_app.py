09d85a686d00a475e16509b0c447d7ed328edffb
EricDiez2702 <147709611+EricDiez2702@users.noreply.github.com>
2023-10-12 12:03:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
+from instagram_basic_display.InstagramBasicDisplay import InstagramBasicDisplay
+
+instagram_basic_display = InstagramBasicDisplay(app_id='631172465595715', app_secret='74c6eb8f791cb341222af420802db667', redirect_url='http://localhost:8501/')
+print(instagram_basic_display.get_login_url())
 
 st.title("The AI knows you better")
