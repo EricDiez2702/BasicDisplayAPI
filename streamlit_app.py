bcca55c704867ab605c26a78828fb3eb3db9d589
EricDiez2702 <147709611+EricDiez2702@users.noreply.github.com>
2023-10-12 15:01:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,26 @@ from instagram_basic_display.InstagramBasicDisplay import InstagramBasicDisplay
 import requests
 
 st.title("Basic Display API")
-instagram_basic_display = InstagramBasicDisplay(app_id='631172465595715', app_secret='74c6eb8f791cb341222af420802db667', redirect_url='https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/')
-st.write(instagram_basic_display.get_login_url())
+#instagram_basic_display = InstagramBasicDisplay(app_id='631172465595715', app_secret='74c6eb8f791cb341222af420802db667', redirect_url='https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/')
+#st.write(instagram_basic_display.get_login_url())
+
+OauthUrl = "https://api.instagram.com/oauth/authorize"
+params = {
+    'client_id':'631172465595715',
+    'redirect_uri':'https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/',
+    'scope':'user_profile,user_media',
+    'response_type':'code'
+}
+
+response = requests.get(OauthUrl, params)
+st.write(response.url)
 
 code = st.text_input("Entrez le code d'authentification :")
 
 code_recu = st.checkbox("Code reçu")
 
 if code_recu:
-    url = "https://api.instagram.com/oauth/access_token"
+    apiUrl = "https://api.instagram.com/oauth/access_token"
     data = {
         "client_id":'631172465595715',
         "client_secret":'74c6eb8f791cb341222af420802db667',
@@ -20,5 +31,5 @@ if code_recu:
         "code":code
     }
     
-    response = requests.post(url, data)
-    st.write(response.json())
+    response1 = requests.post(apiUrl, data)
+    st.write(response1.json())
