6054542afb278596a2b22a96dfeedfd1e44bc91f
EricDiez2702 <147709611+EricDiez2702@users.noreply.github.com>
2023-10-12 14:39:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,15 @@ st.write(instagram_basic_display.get_login_url())
 
 code = st.text_input("Entrez le code d'authentification :")
 
-if code != null:
-    API_URL = "https://api.instagram.com/oauth/access_token"
-    data = {
-        "client_id":'631172465595715',
-        "client_secret":'74c6eb8f791cb341222af420802db667',
-        "grant_type": "authorization_code",
-        "redirect_uri":'https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/',
-        "code":code
-    }
-    
-    response = requests.post(API_URL, data)
-    st.write(response.json())
+API_URL = "https://api.instagram.com/oauth/access_token"
+data = {
+    "client_id":'631172465595715',
+    "client_secret":'74c6eb8f791cb341222af420802db667',
+    "grant_type": "authorization_code",
+    "redirect_uri":'https://basicdisplayapi-kkg3fuibtjua4gvin89ggb.streamlit.app/',
+    "code":code
+}
+
+response = requests.post(API_URL, data)
+st.write(response.json())
 
