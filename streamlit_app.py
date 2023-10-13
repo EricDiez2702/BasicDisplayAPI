252a8cfe2ac7039caa84031e401ef9c93f2234d1
EricDiez2702 <147709611+EricDiez2702@users.noreply.github.com>
2023-10-13 10:59:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import json
 
 st.title("Basic Display API")
 
+//Création de l'URL d'autorisation 
 OauthUrl = "https://api.instagram.com/oauth/authorize"
 params = {
     'client_id':'631172465595715',
@@ -15,10 +16,12 @@ params = {
 response = requests.get(OauthUrl, params)
 st.write(response.url)
 
+//Récupération du code 
 code = st.text_input("Entrez le code d'authentification :")
 
 code_recu = st.checkbox("Code reçu")
 
+//Génération du token à partir du code d'autorisation
 if code_recu:
     apiUrl = "https://api.instagram.com/oauth/access_token"
     data = {
@@ -32,6 +35,7 @@ if code_recu:
     response1 = requests.post(apiUrl, data)
     st.write(response1.json())
 
+    //Récupération du token et de id utilisateur pour accès à son compte
     data = response1.json()
 
     user_id = data.get('user_id')
@@ -46,6 +50,7 @@ if code_recu:
     else:
         st.write(f"La requête a échoué, code d'etat {response2.status_code}.")
 
+    //Récupération de la liste de publication sur compte utilisateur
     mediaListUrl = f'https://graph.instagram.com/{user_id}/media?fields=id,caption,media_type,media_url,timestamp&access_token={access_token}'
     response3 = requests.get(mediaListUrl)
     st.write(type(response3))
@@ -53,15 +58,22 @@ if code_recu:
     if response3.status_code == 200:
         st.write(response3.json())
     else:
-        st.write(f"La requête a échoué, code d'etat {resposne3.status_code}.")
+        st.write(f"La requête a échoué, code d'etat {response3.status_code}.")
 
+    //Récupération des autres photos d'un même album
+    testFirstMediaUrl = f'https://graph.instagram.com/18063413599377238/children?fields=id,media_type,media_url&access_token={access_token}'
+    response4 = requests.get(testFirstMediaUrl)
+    st.write(response4.json())
+
+
+    //Test automatisation de la récupération des albums
     json_obj = json.loads(response3.content)
 
     for i in json_obj['media_type']:
         if i == "CAROUSEL_ALBUM":
             id = json_obj['id']
             mediaUrl = f'https://graph.instagram.com/{id}/children?fields=id,media_type,media_url&access_token={access_token}'
-            response4 = requests.get(mediaUrl)
-            st.write(response4.json())
+            response5 = requests.get(mediaUrl)
+            st.write(response5.json())
 
 
